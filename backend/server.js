c5de93152589eff307e8747f3ac415194e396849
mfatihy70 <mfatihyildiz1453@gmail.com>
2025-02-16 13:06:53 +0100

    heroku integration 2

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import productRoutes from "./routes/product.routes.js"
 import carouselRoutes from "./routes/carousel.routes.js"
 import partnerRoutes from "./routes/partner.routes.js"
 
-
 dotenv.config()
 connectDB()
 const app = express()
@@ -17,10 +16,14 @@ app.use(express.json()) // Parse JSON bodies
 app.use(cors())
 
 // Routes
-app.use("/api/users", userRoutes) // User routes
-app.use("/api/products", productRoutes) // Product routes
-app.use("/api/carousel", carouselRoutes) // Carousel routes
-app.use("/api/partners", partnerRoutes) // Partner routes
+app.use("/api/users", userRoutes)
+app.use("/api/products", productRoutes)
+app.use("/api/carousel", carouselRoutes)
+app.use("/api/partners", partnerRoutes)
+
+app.get("/", (req, res) => {
+  res.json({ message: "Running" })
+})
 
 // Start the server
 const PORT = process.env.PORT || 5000
